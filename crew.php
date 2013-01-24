e7933d58b11207c26b96bbbdc43ef14299ce7818
Graham Thomson <g.thomson@pccl.co.uk>
2013-01-24 16:31:14 +0000

    Initial crew manifest layout (and css)

--- crew.php
+++ crew.php
@@ -1,8 +1,161 @@
 <?php include "includes/header.php"; ?>
 		<div class="maintext">
-			<ul>
-			<li> CO - Lieutenant Commander Ambler Furry</li>
-			<li> CWO - Lieutenant Commander Ethara t'Ral</li>
-			</ul>
+			<table width="100%">
+				<tr>
+					<td colspan="3">
+						<h3>Command</h3>
+					</td>
+				</tr>
+				<tr>
+					<th>Position</th><th>Rank</th><th>Name</th>
+				</tr>
+				<tr>
+					<td>Commanding Officer</td>
+					<td>Lieutenant Commander</td>
+					<td>Ambler Furry</td>
+				</tr>
+				<tr>
+					<td>Executive Officer<br/><span class="subtext">(Chief Weapons Officer)</span></td>
+					<td>Lieutenant Commander</td>
+					<td>Ethara t'Ral</td>
+				</tr>
+				<tr>
+					<td>Chief of the Boat</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td>Quartermaster</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td>Pilot</td>
+					<td>Ensign</td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td colspan="3">
+						<h3>Operations and Science</h3>
+					</td>
+				</tr>
+				<tr>
+					<th>Position</th><th>Rank</th><th>Name</th>
+				</tr>
+				<tr>
+					<td>Chief Operations Officer</td>
+					<td>Lieutenant</td>
+					<td>Guillaume Berest <span class="subtext">[NPC]</span></td>
+				</tr>
+				<tr>
+					<td>Operations Officer</td>
+					<td>Ensign</td>
+					<td>Addyson Gray</td>
+				</tr>
+				<tr>
+					<td>Operations Specialist</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td>Science Specialist</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td>Boatswain</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td>Boatswain's mate</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td>Transporter Chief</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td>Hydroponics Specialist</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td colspan="3">
+						<h3>Engineering</h3>
+					</td>
+				</tr>
+				<tr>
+					<th>Position</th><th>Rank</th><th>Name</th>
+				</tr>
+				<tr>
+					<td>Chief Engineering Officer</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td>Engineering's Mate</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td>Engineering Specialist</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td colspan="3">
+						<h3>Weapons</h3>
+					</td>
+				</tr>
+				<tr>
+					<th>Position</th><th>Rank</th><th>Name</th>
+				</tr>
+				<tr>
+					<td>Chief Weapons Officer<br/><span class="subtext">(Executive Officer)</span></td>
+					<td>Lieutenant Commander</td>
+					<td>Ethara t'Ral</td>
+				</tr>
+				<tr>
+					<td>Weapons Officer</td>
+					<td>Ensign</td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td>Master-At-Arms</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td>Gunner's Mate</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td colspan="3">
+						<h3>Medical</h3>
+					</td>
+				</tr>
+				<tr>
+					<th>Position</th><th>Rank</th><th>Name</th>
+				</tr>
+				<tr>
+					<td>Chief Medical Officer</td>
+					<td>Lieutenant</td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td>Medic</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+				<tr>
+					<td>Nurse</td>
+					<td> -- </td>
+					<td> -- </td>
+				</tr>
+			</table>
 		</div>
 <?php include 'includes/footer.php'; ?>
